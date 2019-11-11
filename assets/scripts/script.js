087611a0cc8fe7a93ddeeba8db7d54ac50582a61
Simon Fraipont <simon.fraipont@gmail.com>
2019-11-11 18:34:49 +0100

    add basic operator functions and barebone html

---
+++ assets/scripts/script.js
@@ -0,0 +1,21 @@
+const add = ( a, b ) => {
+  return a + b;
+}
+
+const subtract = ( a, b ) => {
+  return a - b;
+}
+
+const multiply = ( a, b ) => {
+  return a * b;
+}
+
+const divide = ( a, b ) => {
+  return a / b;
+}
+
+const operate = ( operator, a, b) => {
+  return operator( a, b);
+} 
+
+console.log(add(2, 5));
