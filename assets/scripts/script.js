b9d55a38a2c25a1b9b4e2d39f4bfe577a8abccf5
Simon Fraipont <simon.fraipont@gmail.com>
2019-11-15 16:47:03 +0100

    add basic functionality and multiple operations capabilities

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,100 @@
-const add = ( a, b ) => {
-  return a + b;
+const mathOperations = {
+  add( a, b ) {
+    return parseInt( a ) + parseInt( b );
+  },
+  
+  subtract( a, b ) {
+    return parseInt( a ) - parseInt( b );
+  },
+
+  multiply( a, b ) {
+    return parseInt( a ) * parseInt( b );
+  },
+
+  divide ( a, b ) {
+    return parseInt( a ) / parseInt( b );
+  },
 }
 
-const subtract = ( a, b ) => {
-  return a - b;
+const operate = ( operator, a, b ) => {
+  return mathOperations[operator]( a, b );   
+}; 
+
+const displayNumber = event => {
+  const text = event.target.textContent; 
+  numberEntered = true;
+  operands += text;
+  display.textContent += text;
+};
+
+const registerOperator = event => {
+  if ( event.target.value === "equals" || ! numberEntered ) {
+    return;
+  } 
+  
+  const operator = event.target.value;
+
+  display.textContent += ' ' + event.target.textContent + ' ';
+  operations.push( operands, operator ); 
+  operands = '';
+  numberEntered = false;
 }
 
-const multiply = ( a, b ) => {
-  return a * b;
+const performCalculation = event => {
+  operations.push( operands ); 
+
+  while ( operations.includes( "multiply" ) || operations.includes( "divide" ) ) {
+    operations = parseOperations( operations, "multiply", "divide" );
+  }
+  
+  while ( operations.includes( "add" ) || operations.includes( "subtract" ) ) {
+    operations = parseOperations( operations, "add", "subtract" );
+  }
+  
+  display.textContent = operations;
+  operands = operations[0];
+  operations = [];
+};   
+
+const parseOperations = ( operations, firstOp, secondOp ) => {
+  let operatorIndex = operations.findIndex( element => {
+    return element === firstOp || element === secondOp;
+  });
+
+  if ( operatorIndex !== -1 ) {
+    let newValue = operate( operations[ operatorIndex ], operations[ operatorIndex - 1 ], operations [ operatorIndex + 1 ] );
+    operations.splice( operatorIndex - 1, 3, newValue);
+  }
+  return operations;
+};
+
+const clearAll = event => {
+  numberEntered = false;
+  operations    = [];
+  operands      = '';
+
 }
 
-const divide = ( a, b ) => {
-  return a / b;
-}
+const display         = document.getElementsByClassName( "calculator__display__text" )[0];
+const numberButtons   = document.getElementsByClassName( "calculator__button--number" );
+const operatorButtons = document.getElementsByClassName( "calculator__button--operator" );
+const equalButton     = document.getElementsByClassName( "calculator__button--equal" )[0];
+const clearButton     = document.getElementsByClassName( "calculator__button--clear" )[0]; 
 
-const operate = ( operator, a, b) => {
-  return operator( a, b);
-} 
+let operands        = '';
+let operations      = [];
+let numberEntered   = false;
 
-console.log(add(2, 5));
+// Register event listeners
+[ ...numberButtons ].forEach( number => {
+  number.addEventListener( "click", displayNumber );
+});
+
+[ ...operatorButtons ].forEach( operator => {
+  operator.addEventListener( "click", registerOperator );
+});
+
+equalButton.addEventListener( "click", performCalculation );
+
+clearButton.addEventListener( "click", clearAll );
+
